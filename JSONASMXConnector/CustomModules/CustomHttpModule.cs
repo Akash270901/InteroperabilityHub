6762ad0953903f165d98afdada7cd1065a5e6e90
Akash <108666491+Akash2709204@users.noreply.github.com>
2024-04-30 14:56:55 +0530

    Comments Added and handled remote Exception message

--- JSONASMXConnector/CustomModules/CustomHttpModule.cs
+++ JSONASMXConnector/CustomModules/CustomHttpModule.cs
@@ -5,35 +5,62 @@ using System.Configuration;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Xml;
 using System.Xml.Linq;
 
 namespace JSONASMXConnector.CustomModules
 {
+    /// <summary>
+    /// Custom HTTP module for processing requests.
+    /// </summary>
     public class CustomHttpModule : IHttpModule
     {
-        private static object lockObject = new object();
+        private static readonly object lockObject = new object();
+
+        /// <summary>
+        /// Initialize the HTTP module.
+        /// </summary>
+        /// <param name="context">The HttpApplication instance.</param>
         public void Init(HttpApplication context)
         {
             context.BeginRequest += OnBeginRequest;
             context.EndRequest += OnEndRequest;
         }
 
-        public void Dispose()
-        {
-        }
+        public void Dispose() { }
 
+        /// <summary>
+        /// Event handler for incoming requests.
+        /// </summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event arguments.</param>
         private void OnBeginRequest(object sender, EventArgs e)
         {
+            // Ensure thread safety with a lock
             lock (lockObject)
             {
-                bool IsAuthorized = false;
                 var application = (HttpApplication)sender;
+                var httpResponse = application.Response;
+
+                // Add security headers
+                httpResponse.AddHeader("Content-Security-Policy", "default-src 'self'");
+                httpResponse.AddHeader("X-Content-Type-Options", "nosniff");
+                httpResponse.AddHeader("X-Frame-Options", "DENY");
+                httpResponse.AddHeader("X-XSS-Protection", "1; mode=block");
+
                 var request = application.Request;
+                string endpoint = HttpContext.Current.Request.Path;
+
+                // Skip processing for initialization requests
+                if (IsProjectInitializationRequest(endpoint))
+                    return;
+
+                bool IsAuthorized = false;
                 string authorizationToken = request.Headers["Authorization"];
                 string ipAddress = request.UserHostAddress;
-                string endpoint = HttpContext.Current.Request.Path;
+
                 int lastSlashIndex = endpoint.LastIndexOf('/');
                 string api_ver = string.Empty;
                 if (lastSlashIndex > 0)
@@ -48,7 +75,7 @@ namespace JSONASMXConnector.CustomModules
                 }
 
                 string token1 = ConfigurationManager.AppSettings["token1"];
-                string path = System.Web.Hosting.HostingEnvironment.MapPath($"~/App_Data/Transmissions/NEOS-CustomerAPI/{api_ver}");
+                string path = System.Web.Hosting.HostingEnvironment.MapPath($"{ConfigurationManager.AppSettings["loggingPath"]}/{api_ver}");
                 string serviceurl = ConfigurationManager.AppSettings["Serviceurl"];
                 serviceurl += api_ver + ".asmx";
                 HttpWebRequest newrequest = (HttpWebRequest)WebRequest.Create(serviceurl);
@@ -57,6 +84,8 @@ namespace JSONASMXConnector.CustomModules
                 {
                     if (token1 == authorizationToken)
                         IsAuthorized = true;
+
+                    // Process the request body
                     using (var reqbody = new StreamReader(request.InputStream))
                     {
                         var Body = reqbody.ReadToEnd();
@@ -70,100 +99,142 @@ namespace JSONASMXConnector.CustomModules
                             ClearAndWriteJsonResponse(application, ConfigurationManager.AppSettings["Error:ContentType"]);
                             return;
                         }
+
+                        // Deserialize JSON body to JObject
                         JObject jObject = Newtonsoft.Json.JsonConvert.DeserializeObject<JObject>(Body);
                         ModifyNullValues(jObject);
                         var xmlBody = ConvertJObjectToXml(jObject);
 
+                        // Convert JSON to SOAP XML
                         string soapXml = ConvertToSoapXml(xmlBody, endpoint, api_ver);
                         xmlDoc.LoadXml(soapXml);
 
+                        // Set up HTTP request
                         newrequest.Method = request.HttpMethod;
                         newrequest.ContentType = "application/soap+xml";
+
+                        // Write SOAP XML to request stream
                         using (StreamWriter writer = new StreamWriter(newrequest.GetRequestStream()))
                         {
                             writer.Write(xmlDoc.InnerXml);
                         }
-                        using (HttpWebResponse response = (HttpWebResponse)newrequest.GetResponse())
+
+                        // Get response from the service
+                        try
                         {
-                            using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                            using (HttpWebResponse response = (HttpWebResponse)newrequest.GetResponse())
                             {
-                                string xmlResponse = reader.ReadToEnd();
-                                var xDoc = XDocument.Parse(xmlResponse);
+                                using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                                {
+                                    string xmlResponse = reader.ReadToEnd();
+                                    var xDoc = XDocument.Parse(xmlResponse);
 
-                                var converter = new Newtonsoft.Json.Converters.XmlNodeConverter { OmitRootObject = true };
-                                var rootToken = JObject.FromObject(xDoc, Newtonsoft.Json.JsonSerializer.CreateDefault(new Newtonsoft.Json.JsonSerializerSettings { Converters = { converter } }))
-                                    .ReplaceXmlNilObjectsWithNull();
+                                    // Convert XML response to JSON
+                                    var converter = new Newtonsoft.Json.Converters.XmlNodeConverter { OmitRootObject = true };
+                                    var rootToken = JObject.FromObject(xDoc, Newtonsoft.Json.JsonSerializer.CreateDefault(new Newtonsoft.Json.JsonSerializerSettings { Converters = { converter } }))
+                                        .ReplaceXmlNilObjectsWithNull();
 
-                                var resultEnvelope = rootToken["soap:Envelope"];
-                                var bodyResponse = rootToken["soap:Body"];
-                                JObject finalObj;
-                                var result = bodyResponse[$"{endpoint}Response"][$"{endpoint}Result"];
-                                if (result != null && result.Type == JTokenType.Object)
-                                {
-                                    finalObj = (JObject)result;
-                                }
-                                else
-                                {
-                                    finalObj = new JObject();
-                                }
-                                string finalResult = finalObj.ToString();
-                                var jsonRequestDetails = new
-                                {
-                                    Request = new
+                                    var resultEnvelope = rootToken["soap:Envelope"];
+                                    var bodyResponse = rootToken["soap:Body"];
+                                    JObject finalObj;
+
+                                    // Extract the specific result from the SOAP response
+                                    var result = bodyResponse[$"{endpoint}Response"][$"{endpoint}Result"];
+                                    if (result != null && result.Type == JTokenType.Object)
                                     {
-                                        request.HttpMethod,
-                                        request.ContentType,
-                                        Endpoint = endpoint,
-                                        AuthorizationToken = authorizationToken
-                                    },
-                                    Client = new
+                                        finalObj = (JObject)result;
+                                    }
+                                    else
                                     {
-                                        IPAddress = ipAddress
-                                    },
-                                    Time = DateTime.UtcNow,
-                                    Response = new
+                                        finalObj = new JObject();
+                                    }
+                                    string finalResult = finalObj.ToString();
+                                    var jsonRequestDetails = new
                                     {
-                                        finalObj
-                                    },
-                                    Authorized = IsAuthorized
-                                };
-                                if (!Directory.Exists(path))
-                                {
-                                    Directory.CreateDirectory(path);
+                                        Request = new
+                                        {
+                                            request.HttpMethod,
+                                            request.ContentType,
+                                            Endpoint = endpoint,
+                                            AuthorizationToken = authorizationToken
+                                        },
+                                        Client = new
+                                        {
+                                            IPAddress = ipAddress
+                                        },
+                                        Time = DateTime.UtcNow,
+                                        Response = new
+                                        {
+                                            finalObj
+                                        },
+                                        Authorized = IsAuthorized
+                                    };
+                                    if (!Directory.Exists(path))
+                                    {
+                                        Directory.CreateDirectory(path);
+                                    }
+                                    string formattedDateTime = DateTime.UtcNow.ToString("yyyy-MM-dd-hh-mm-ss");
+
+                                    string xmlLogPath = Path.Combine(path, $"{formattedDateTime}-{endpoint}.xml");
+
+                                    // Log the XML request and response
+                                    using (XmlWriter writer = XmlWriter.Create(xmlLogPath))
+                                    {
+                                        writer.WriteStartElement($"{endpoint}");
+                                        writer.WriteElementString("FullRequest", xmlDoc.InnerXml.ToString());
+                                        writer.WriteStartElement("Request");
+                                        writer.WriteElementString("HttpMethod", newrequest.Method);
+                                        writer.WriteElementString("ContentType", newrequest.ContentType);
+                                        writer.WriteElementString("Endpoint", endpoint);
+                                        writer.WriteElementString("AuthorizationToken", authorizationToken);
+                                        writer.WriteEndElement();
+                                        writer.WriteStartElement("Client");
+                                        writer.WriteElementString("IPAddress", ipAddress);
+                                        writer.WriteEndElement();
+                                        writer.WriteElementString("Time", DateTime.UtcNow.ToString());
+                                        writer.WriteElementString("Response", xmlResponse);
+                                        writer.WriteElementString("Authorized", IsAuthorized.ToString());
+                                        writer.WriteEndElement();
+                                    }
+
+                                    // Serialize JSON request details and log as JSON
+                                    string jsondata = Newtonsoft.Json.JsonConvert.SerializeObject(jsonRequestDetails, Newtonsoft.Json.Formatting.Indented);
+                                    string jsonLogPath = Path.Combine(path, $"{formattedDateTime}-{endpoint}.json");
+                                    File.WriteAllText(jsonLogPath, jsondata);
+                                    ClearAndWriteJsonResponse(application, finalResult);
                                 }
-                                string formattedDateTime = DateTime.UtcNow.ToString("yyyy-MM-dd-hh-mm-ss");
-
-                                string xmlLogPath = Path.Combine(path, $"{formattedDateTime}-{endpoint}.xml");
-
-                                using (XmlWriter writer = XmlWriter.Create(xmlLogPath))
-                                {
-                                    writer.WriteStartElement($"{endpoint}");
-                                    writer.WriteElementString("FullRequest", xmlDoc.InnerXml.ToString());
-                                    writer.WriteStartElement("Request");
-                                    writer.WriteElementString("HttpMethod", newrequest.Method);
-                                    writer.WriteElementString("ContentType", newrequest.ContentType);
-                                    writer.WriteElementString("Endpoint", endpoint);
-                                    writer.WriteElementString("AuthorizationToken", authorizationToken);
-                                    writer.WriteEndElement();
-                                    writer.WriteStartElement("Client");
-                                    writer.WriteElementString("IPAddress", ipAddress);
-                                    writer.WriteEndElement();
-                                    writer.WriteElementString("Time", DateTime.UtcNow.ToString());
-                                    writer.WriteElementString("Response", xmlResponse);
-                                    writer.WriteElementString("Authorized", IsAuthorized.ToString());
-                                    writer.WriteEndElement();
-                                }
-
-                                string jsondata = Newtonsoft.Json.JsonConvert.SerializeObject(jsonRequestDetails, Newtonsoft.Json.Formatting.Indented);
-                                string jsonLogPath = Path.Combine(path, $"{formattedDateTime}-{endpoint}.json");
-                                File.WriteAllText(jsonLogPath, jsondata);
-                                ClearAndWriteJsonResponse(application, finalResult);
                             }
+                        }
+                        catch (WebException webEx)
+                        {
+                            // If the server returns an error, read the error response
+                            string errorResponseText = string.Empty;
+                            if (webEx.Response != null)
+                            {
+                                using (var errorResponse = (HttpWebResponse)webEx.Response)
+                                using (var errorReader = new StreamReader(errorResponse.GetResponseStream()))
+                                {
+                                    var errorText = errorReader.ReadToEnd();
+                                    string pattern = @"System\.Exception:\s*\d+:\s*(.*?)\.\s*([\d\.]+)";
+                                    Match match = Regex.Match(errorText, pattern);
+                                    if (match.Success)
+                                    {
+                                        string errorMessage = match.Groups[1].Value;
+                                        string errIP = match.Groups[2].Value;
+                                        errorResponseText = $"{errorMessage}. {errIP}";
+                                    }
+                                }
+                            }
+                            else
+                                errorResponseText = webEx.Message;
+
+                            throw new Exception(errorResponseText, webEx);
                         }
                     }
                 }
                 catch (Exception ex)
                 {
+                    // Create JSON response for the exception
                     var jsonRequestDetails = new
                     {
                         Request = new
@@ -194,6 +265,7 @@ namespace JSONASMXConnector.CustomModules
                     string xmlLogFileName = $"{formattedDateTime}-{endpoint}.xml";
                     string xmlLogPath = Path.Combine(path, xmlLogFileName);
 
+                    // Log the XML request and error details
                     using (XmlWriter writer = XmlWriter.Create(xmlLogPath))
                     {
                         writer.WriteStartElement($"{endpoint}");
@@ -216,9 +288,12 @@ namespace JSONASMXConnector.CustomModules
                         writer.WriteEndElement();
                     }
 
+                    // Serialize JSON request details and log as JSON
                     string jsondata = Newtonsoft.Json.JsonConvert.SerializeObject(jsonRequestDetails, Newtonsoft.Json.Formatting.Indented);
+                    string jsonLogPath = Path.Combine(path, $"{formattedDateTime}-{endpoint}.json");
+                    File.WriteAllText(jsonLogPath, jsondata);
 
-                    File.WriteAllText(path + $"{formattedDateTime}-{endpoint}.json", jsondata);
+                    // Respond to the client with the error message
                     ClearAndWriteJsonResponse(application, ex.Message.ToString());
                 }
             }
@@ -226,6 +301,7 @@ namespace JSONASMXConnector.CustomModules
 
         private void ClearAndWriteJsonResponse(HttpApplication application, dynamic bodyResponse)
         {
+            // Clear the existing response, set content type to JSON, and write the response body
             application.Response.Clear();
             application.Response.ContentType = "application/json";
             application.Response.Write(bodyResponse);
@@ -307,7 +383,13 @@ namespace JSONASMXConnector.CustomModules
                 }
             }
         }
-
+        private bool IsProjectInitializationRequest(string endpoint)
+        {
+            // Skip requests to initialization URLs
+            return endpoint.Equals("/", StringComparison.OrdinalIgnoreCase) ||
+                   endpoint.Equals("/index.html", StringComparison.OrdinalIgnoreCase) ||
+                   endpoint.Equals("/favicon.ico", StringComparison.OrdinalIgnoreCase);
+        }
         private void OnEndRequest(object sender, EventArgs e)
         {
         }
